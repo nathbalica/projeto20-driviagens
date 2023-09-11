b3b3ac47c101057309dcce437473354a715a2e0c
nathbalica <nathbalica@gmail.com>
2023-09-11 14:14:03 -0300

    configDatabase

--- src/database/database.js
+++ src/database/database.js
@@ -4,18 +4,20 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const db = new Pool({
+const configDatabase = {
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "driven",
   database: process.env.DB_DATABASE || "driviagens"
-});
+};
 
 if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 
-export default db;
+export const db = new Pool(configDatabase);
+
+// export default db;
 
 db.query("SELECT 1")
   .then(() => {
